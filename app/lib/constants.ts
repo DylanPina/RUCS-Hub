9644d066a49e7bb814ad1ee55496f40f09ce7e25
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-01-10 19:48:31 -0500

    Added parseSynposesListing

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,4 +1,7 @@
 export const WEBREG_BASE_URL =
   "https://sis.rutgers.edu/oldsoc/courses.json?subject=198";
+export const COURSE_SYNOPSES_URL =
+  "https://www.cs.rutgers.edu/academics/undergraduate/course-synopses";
+export const RUTGERS_CS_URL = "https://www.cs.rutgers.edu/";
 export const MAX_YEAR = 2024;
 export const MIN_YEAR = 2023;
--- app/lib/data/course.ts
+++ app/lib/data/course.ts
@@ -1,15 +1,16 @@
-import { Course, Term } from "@/app/lib/definitions/course";
+import { Term } from "@/app/lib/definitions/course";
 import { validateCourseTermYear } from "@/app/lib/utils";
-import { WEBREG_BASE_URL } from "@/app/lib/constants";
+import { COURSE_SYNOPSES_URL, RUTGERS_CS_URL } from "@/app/lib/constants";
+import { JSDOM } from "jsdom";
 
 /**
  * Fetches all courses from Rutgers CS webreg listing for a given semester.
  *
  * @param year - Year the course is/was offered (2022 - 2024)
  * @param term - Term it is/was offered
- * @returns - List of courses offered for that specific year and term
+ * @return - List of courses offered for that specific year and term
  */
-export async function fetchCourses(year: number, term: Term) {
+export async function fetchCourseListings(year: number, term: Term) {
   try {
     validateCourseTermYear(year, term);
   } catch (error) {
@@ -17,20 +18,69 @@ export async function fetchCourses(year: number, term: Term) {
     throw new Error(`Failed to fetch courses: ${error}`);
   }
 
-  const endpoint = `${WEBREG_BASE_URL}&semester=${term.valueOf()}${year}&campus=NB&level=UG`;
-  const res = await fetch(endpoint);
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch courses data from: ${endpoint}`);
-  }
-
-  return res.json();
+  return parseSynposesListing(COURSE_SYNOPSES_URL);
 }
 
 /**
- * Parses the raw JSON data for the courses fetched from the webreg API into an array of Course objects
+ * Parses the HTML for the course synposes listing to return a list of course names and their IDs
  *
- * @param courseJson - Raw course JSON data
- * @returns - List of Course objects
+ * @param courseSynposesHtml - HTML for the course synposes website
+ * @return - List of course names and IDs
  */
-function parseRawCourseJson(courseJson: JSON): Course[] {}
+export async function parseSynposesListing(url: string): Promise<any[]> {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching synposes listing from ${url}`);
+    }
+
+    const html = await response.text();
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    const courseElements = document.querySelectorAll(
+      ".latestnews-item .newstitle a",
+    );
+
+    const courses: any[] = [];
+    courseElements.forEach((element: Element) => {
+      const courseString = element.textContent?.trim() || "";
+      const synopsisLink = `${RUTGERS_CS_URL}${
+        element.getAttribute("href") || ""
+      }`;
+
+      const [courseCode, courseName] = parseCourseSynposesString(courseString);
+      courses.push({ courseCode, courseName, synopsisLink });
+    });
+
+    console.log(courses);
+    return courses;
+  } catch (error) {
+    console.error(`Error parsing synposes listing from ${url}: ${error}`);
+    return [];
+  }
+}
+
+/**
+ * Parses course the Rutgers CS synposes listing string (ex: "01:198:103 - Introduction to Computer Skills")
+ *
+ * @param courseSynposesString - String rerepesnting the listing on course synposes list
+ * @return - Parsed course ID and course title (ex [103, "Introduction to Computer Skills"])
+ */
+function parseCourseSynposesString(courseString: string): [number, string] {
+  const normalizedString = courseString.replace(/-/g, " - ");
+
+  let parts = normalizedString.split(/\s-\s/);
+
+  if (parts.length === 1) {
+    parts = normalizedString.split(" ");
+  }
+
+  const idPart = parts.shift();
+  const match = idPart ? idPart.match(/\d{3}(?=\D*$)/) : null;
+  const courseCode = match ? parseInt(match[0]) : -1;
+  const courseName = parts.join(" ").trim();
+
+  return [courseCode, courseName];
+}
