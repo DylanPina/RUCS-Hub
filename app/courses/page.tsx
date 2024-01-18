d8da32c18b0cb8b322ab6d87033e672ede48931a
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-01-18 18:12:00 -0500

    Added fetchCourseTableData

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,11 +1,6 @@
 import CourseTable from "@/components/course-table/course_table";
 import CourseListingHeader from "@/components/course-table/course_table_header";
-import {
-  fetchAllCourseTableListings,
-  fetchCourseTableListingByYear,
-  fetchCourseTableListingsByTerm,
-  fetchCourseTableListingsByYearTerm,
-} from "@/lib/data/course";
+import { fetchCourseTableData } from "@/lib/data/course";
 import { CourseTableColumn } from "@/lib/definitions/course";
 import { getTermByName } from "@/lib/utils";
 
@@ -14,22 +9,10 @@ export default async function Page({ searchParams }: { searchParams: string }) {
   const year = params.get("year") ? Number(params.get("year")) : null;
   const term = getTermByName(params.get("term") || "");
 
-  console.log(`Year from params: ${params.get("year")}`);
-
-  console.log(`Year: ${year}`);
-  console.log(`Term: ${term}`);
-
-  let courseTableData: CourseTableColumn[] = [];
-  if (year !== null && term !== null) {
-    courseTableData = await fetchCourseTableListingsByYearTerm(year, term);
-  } else if (term !== null && year === null) {
-    courseTableData = await fetchCourseTableListingsByTerm(term);
-  } else if (term === null && year !== null) {
-    courseTableData = await fetchCourseTableListingByYear(year);
-    console.log("Year");
-  } else {
-    courseTableData = await fetchAllCourseTableListings();
-  }
+  const courseTableData: CourseTableColumn[] = await fetchCourseTableData(
+    year,
+    term,
+  );
 
   return (
     <div className="flex flex-col place-items-center justify-center space-y-4">
