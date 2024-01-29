dbf237943b28f7071a47eef94ccc0e6ec6459d03
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-01-29 13:02:18 -0500

    Updated schema definitions

--- lib/definitions/user.ts
+++ lib/definitions/user.ts
@@ -1,9 +1,8 @@
 import { Review } from "./review";
 
-export type Student = {
-  studentId: string;
-  email?: string;
-  displayName?: string;
-  photoUrl?: string;
+export type User = {
+  id: number;
+  createdAt: Date;
+  email: string;
   reviews?: Review[];
 };
