2a8bd7cc99ecf5255d1749cfcbda79c6f3145d2e
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-01-31 12:35:20 -0500

    Added seedProfessors

--- lib/mock-data/review-mock-data.ts
+++ lib/mock-data/review-mock-data.ts
@@ -1,3 +1,54 @@
-import { Review } from "../definitions/review";
+import { Term } from "../definitions/course";
 
-export const reviewMockData: Review[] = [];
+export const mockReviews: any[] = [
+  {
+    userId: 1,
+    semester: Term.Spring,
+    year: 2024,
+    courseCode: 462,
+    title: "Both fair and reasonable",
+    content: "Machine learning is all about punishment",
+    professorId: 18,
+    rating: 10,
+  },
+  {
+    userId: 1,
+    semester: Term.Spring,
+    year: 2024,
+    courseCode: 462,
+    title: "Where the fuck is bagel bot?",
+    content: "No bagel?",
+    professorId: 18,
+    rating: 2,
+  },
+  {
+    userId: 1,
+    semester: Term.Fall,
+    year: 2023,
+    courseCode: 440,
+    title: "Best course I've ever taken",
+    content: "Introductory to florida man theory",
+    professorId: 18,
+    rating: 10,
+  },
+  {
+    userId: 1,
+    semester: Term.Spring,
+    year: 2023,
+    courseCode: 206,
+    title: "A's were handed out like candy",
+    content: "Dr. Cowan is truly a menance to society",
+    professorId: 18,
+    rating: 8,
+  },
+  {
+    userId: 1,
+    semester: Term.Summer,
+    year: 2022,
+    courseCode: 205,
+    title: "Homework made me cry",
+    content: "Homework was incredibly but ended up with an A",
+    professorId: 18,
+    rating: 9,
+  },
+];
---
+++ prisma/migrations/20240131022257_v1_3/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Review" ADD COLUMN     "rating" INTEGER;
