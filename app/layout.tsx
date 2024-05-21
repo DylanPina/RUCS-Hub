0360afb11baf2371abe83558545bc4cc5fec0184
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-05-21 17:16:37 -0400

    Added opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "@/components/auth/auth_provider";
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "@/components/shadcn/ui/toaster";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 const font = Source_Code_Pro({ subsets: ["latin"] });
 
@@ -24,6 +25,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Courses | RUCS Hub</title>
+        <meta
+          name="description"
+          content="Unoffical hub for Rutger's computer science"
+        />
+
+        <meta property="og:url" content="https://www.rucshub.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="RUCS Hub" />
+        <meta
+          property="og:description"
+          content="Unoffical hub for Rutger's computer science"
+        />
+        <meta property="og:image" content="" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="rucshub.com" />
+        <meta property="twitter:url" content="https://www.rucshub.com" />
+        <meta name="twitter:title" content="Courses | RUCS Hub" />
+        <meta
+          name="twitter:description"
+          content="Unoffical hub for Rutger's computer science"
+        />
+        <meta name="twitter:image" content="" />
+      </Head>
       <body className={font.className}>
         <AuthProvider>
           <div className="flex flex-col place-items-center min-h-screen bg-zinc-900 overflow-auto">
